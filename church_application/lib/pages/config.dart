08604da8e42be745dc15ee01245277a728052a17
durdunela <durdunela@gmail.com>
2024-09-15 14:00:11 +0400

    phone test

--- church_application/lib/pages/config.dart
+++ church_application/lib/pages/config.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  // Use your machine's local IP address here
   final String baseUrl = 'http://192.168.1.28:3000';
 
   final String registrationEndpoint = '/registration';
